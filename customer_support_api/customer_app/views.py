b34006069f0df55781f35381501499c4b9391119
chirayurathi <chirayurathi4@gmail.com>
2022-06-25 15:52:23 +0530

    dummy chat request

--- customer_support_api/customer_app/views.py
+++ customer_support_api/customer_app/views.py
@@ -1,7 +1,20 @@
-from urllib import request
 from django.http import JsonResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+def dummy(req):
+    return("answer to : " + req)
+
+@api_view(['POST'])
 def Test(request):
-    return JsonResponse({"data":"test"})
+    try:
+        data = request.data['message']
+        return JsonResponse({
+            "success":True,
+            "data":dummy(data)
+        })
+    except:
+        return JsonResponse({
+            "success":False,
+            "error":"invalid data"
+        })
