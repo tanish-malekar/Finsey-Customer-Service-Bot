700a3a9ed94399a8e60f9d7e909754eb71230d5d
chirayurathi <chirayurathi4@gmail.com>
2022-06-25 11:50:23 +0530

    pages added

---
+++ customer-support/src/pages/landing-page.js
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import LandingMaster from "../Components/LandingMaster";
+import PrimaryNav from "../Components/PrimaryNav";
+import SecondaryNav from "../Components/SecondaryNav";
+const LandingPage = () => {
+  return (
+    <Grid container>
+      
+    </Grid>
+  );
+};
+
+export default LandingPage;
